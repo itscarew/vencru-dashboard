2b2546c7806c1c59464113b77cf806df0bf734d3
itscarew <itscarew@gmail.com>
2023-01-10 23:04:05 +0100

    updated the Input components

--- components/Input.tsx
+++ components/Input.tsx
@@ -12,7 +12,7 @@ export default function Input() {
             <div className='flex items-center relative mb-4' >
                 <input
                     type={"text"}
-                    className={`w-full border-2 rounded-lg py-2 pl-10 border-gray-300 focus:outline-none  focus:border-gray-400 `}
+                    className={`w-full border-2 outline-none rounded-lg py-2 pl-10 border-gray-300 focus:outline-none  focus:border-gray-400 `}
                     placeholder='Search'
                     value={data}
                     onChange={handleChange}
